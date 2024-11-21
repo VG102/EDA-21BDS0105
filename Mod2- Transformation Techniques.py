94447d0b1755c716d87112c5b96427e36b489e26
VG102 <90925587+VG102@users.noreply.github.com>
2024-11-21 21:56:52 +0530

    Update Mod2- Transformation Techniques.py
    Transformation Techniques: Performing Data Deduplication

--- Mod2- Transformation Techniques.py
+++ Mod2- Transformation Techniques.py
@@ -12,12 +12,3 @@ data_cleaned_dedup = data.drop_duplicates()
 
 print("\n=== Data After Deduplication ===")
 print(data_cleaned_dedup.head())
-
-# Replacing specific values
-data_cleaned_dedup['Address'] = data_cleaned_dedup['Address'].replace('Old Address', 'New Address')
-
-# Example: Replace values in 'Price' column where price < 100000 with 'Low Price'
-data_cleaned_dedup['Price'] = data_cleaned_dedup['Price'].apply(lambda x: 'Low Price' if x < 100000 else x)
-
-print("\n=== Data After Value Replacement ===")
-print(data_cleaned_dedup.head())
